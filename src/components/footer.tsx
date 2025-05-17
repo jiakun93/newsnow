813ab83ed2a21d26c891499f981e952a080a2047
jiakun93 <156577404+jiakun93@users.noreply.github.com>
2025-05-17 18:37:48 +0800

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,9 @@
 export function Footer() {
+  const currentYear = new Date().getFullYear(); // 自动获取当前年份
+  
   return (
     <>
+      <div>天灰灰会不会，让我忘记你是谁</div> {/* 页脚一言 */}
       <a href={`${Homepage}/blob/main/LICENSE`} target="_blank">MIT LICENSE</a>
       <span>
         <span>NewsNow © 2024 By </span>
