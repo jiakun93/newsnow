6c0c44eead832de01d51b447b2ff676c627ee22d
heak <jiakun93@qq.com>
2026-04-03 03:34:46 +0800

    Add footer message for DigitalPlat FreeDomain
    Added a footer message promoting DigitalPlat FreeDomain.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,7 @@
 export function Footer() {
   return (
     <>
+      <div>This Website is Powered by DigitalPlat FreeDomain,Get a free domain from DigitalPlat.</div> {/* 页脚一言 */}
       <a href={`${Homepage}/blob/main/LICENSE`} target="_blank">MIT LICENSE</a>
       <span>
         <span>NewsNow © 2024 By </span>
